daa5f8583aad89e922fcdb7767e801147a95b65c
r-chi-ba <92197953+r-chi-ba@users.noreply.github.com>
2023-07-02 00:14:45 +0900

    Update stock_func.py

--- stock_func.py
+++ stock_func.py
@@ -1,176 +1,252 @@
-import os
-import pandas_datareader as data
+from pandas_datareader import data as pdr
+import datetime
+import yfinance as yf 
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from datetime import date as dt
-from dateutil.relativedelta import relativedelta as rdelta
-#import argparse
-#%matplotlib inline
-# pd.core.common.is_list_like = pd.api.types.is_list_like
 
-'''
-def parse_args():
+class DaySeries:
     
-    parser = argparse.ArgumentParser(description='Plot or analyze stock price')
-    parser.add_argument('--name', default='N225', help='name of brand, and it is used as path too.', type=str)
-    parser.add_argument('--site', default='yahoo', help='Web scraping (Where the data comes from)', type=str)
-    parser.add_argument('--code', default='^N225', help='code of the brand')
-    parser.add_argument('--te',default=dt.today().isoformat(), help='End date', type=str)
-    parser.add_argument('--ts', default=(dt.today()+rdelta(months=-1)).isoformat(), help='Start date', type=str)
-    parser.add_argument('--showfig', default=False, help='Whether figures are shown or not(default=False).', type=bool)
-    parser.add_argument('--savefig', default=True, help='Whether figures are saved or not(default=True).', type=bool)
-    parser.add_argument('--path', default='pictures', help='path where figures save')
-    parser.add_argument('--md', default=10, help='How many days are averaged', type=int)
-    
-    return parser.parse_args()
-'''
-
-class Stock:
-    
-    name = input("Brand name: ")
-    #Brand name, and it is used as path too.
-    if name == "":
-        name = "N225"
-
-    site = input("Web site: ")
-    #Web scraping (Where the data comes from)
-    if site == "":
-        site = "yahoo"
-    
-    code = input("Code: ")
-    #code of the brand
-    if code == "":
-        code = "^N225"
-    
-    ts = input("Start date: ")
-    #default is a month ago from today.
-    if ts == "":
-        ts = (dt.today()+rdelta(months=-1)).isoformat()
+    def __init__(self, ticker, start, end):
         
-    te = input("End date: ")
-    #default is today.
-    if te == "":
-        te = dt.today().isoformat()
-        
-    savefig = input("If save figures or not, T or F. (default=F): ")
-    if savefig == "T":
-        savefig = True
-    else:
-        savefig = False
-        
-    showfig = input("If show figures or not, T or F. (default=T): ")
-    if showfig == "F":
-        showfig = False
-    else:
-        showfig = True
-        
-    path = "pictures"
-        
-    df = data.DataReader(code, site, ts, te)
-    #scriping data
+        self.ticker = ticker
+        self.start = start
+        self.end = end
+        """
+        start = "2023-1-1" #@param {type:"string"}
+        end = datetime.date.today()
+        ticker = "AAPL" #@param [""] {allow-input: true}
+        """
+        # データ取得をdrからyfinanceへ変更。2021.7.4
+        yf.pdr_override() # <== that's all it takes :-)
+        # download dataframe
+        self.df = pdr.get_data_yahoo(ticker, start, end)
     
-    def graph(self):
+    """
+    def df(self):
+        df = self.df
+        return df
+    """
+    
+    def open_mean(self):
+        mean = self.df["Open"].mean()
+        return mean
+    
+    def close_mean(self):
+        mean = self.df["Close"].mean()
+        return mean
+    
+    def adj_colse_mean(self):
+        mean = self.df["Adj Close"].mean()
+        return mean
+    
+    def high_mean(self):
+        mean = self.df["High"].mean()
+        return mean
+    
+    def low_mean(self):
+        mean = self.df["Low"].mean()
+        return mean
+    
+    def volume_mean(self):
+        mean = self.df["Volume"].mean()
+        return mean
+    
+    def close_std(self):
+        std = self.df["Close"].std()
+        return std
+    
+    def adj_close_std(self):
+        std = self.df["Adj Close"].std()
+        return std
+    
+    def high_std(self):
+        std = self.df["High"].std()
+        return std
+    
+    def low_std(self):
+        std = self.df["Low"].std()
+        return std
+    
+    def close_std(self):
+        std = self.df["close"].std()
+        return std
+    
+    def plot(self, md=10):
+        from matplotlib import pyplot as plt
         
         df = self.df
         
-        md = input("Mean days (int, default=10): ")
-        #How many days are averaged.
-        if md == "":
-            md = 10
         price = df['Adj Close']
         date = df.index
-        price = price
-        date = date
         
         df[str(md)+'mean'] = price.rolling(window=int(md)).mean()
         
         plt.figure(figsize=(30, 15))
         plt.subplot(3,1,1)
 
-        plt.title(self.name, fontsize=18)
-        plt.xlabel('date', fontsize=18)
-        plt.ylabel('price', fontsize=18)
+        plt.title(self.ticker, fontsize=18)
+        plt.ylabel('Price', fontsize=18)
 
         plt.plot(date, price, label='Adj Close', color='#99b898')
         plt.plot(date, df[str(md)+'mean'], label=str(md)+'days mean', color='#e84a5f')
         plt.legend(fontsize=18)
 
         plt.subplot(3,1,2)
+        plt.ylabel('Volume', fontsize=18)
         plt.bar(date, df['Volume'], label='Volume', color='gray')
         plt.legend(fontsize=18)
     
         plt.subplot(3,1,3)
-        plt.plot(date, price, label=['Adj Close'], color='r')
+        plt.plot(date, price, label='Adj Close', color='r')
         plt.plot(date, df['High'], label='High', color='b')
         plt.plot(date, df['Low'], label='Low', color='b')
+        plt.ylabel('Price', fontsize=18)
+        plt.xlabel('Date', fontsize=18)
         plt.legend(fontsize=18)
         plt.plot()
-        if self.savefig == True:
-            os.makedirs(self.path + "/" + self.name, exist_ok = True)
-            plt.savefig(self.path + '/'  + self.name + "/" + '.png')
-        if self.showfig == True:
-            plt.show()
-    
-    def diff(self):
-    
+        plt.show()
+        
+    def cross_point(self, md=10):
         df = self.df
-        tren = [0]
-        raise_rate = [0]
-        widt = []
+        
+        if len(df) < md + 1:
+            print("The length of data is required more than " + str(md + 1) + ".")
+            return
+        
+        else:
+            price = df['Adj Close']
+            date = df.index
+            means = price.rolling(window=int(md)).mean()
+            up = []
+            down = []
+            for i in range(md, len(means)-1):
+                
+                if price[i] <= means[i] and price[i+1] > means[i+1]:
+                    up.append(i)
+                elif price[i] >= means[i] and price[i+1] < means[i+1]:
+                    down.append(i)
+           
+            return (up, down, md)
+    
+    def peak_rate(self, md=10, key="Adj Close"):
+        up, down, md = self.cross_point(md)
+        df = self.df
+        up_rates = []
+        durations = []
+        cross = []
+        peak = []
+        if up[0] > down[0]:   
+            for i in range(len(up)):
+                
+                date = df.index[up[i]:down[i+1]]
+                ac = df[key][up[i]:down[i+1]]
+                pre = ac[0]
+                max_value = ac.max()
+                idxmax = ac.idxmax()
 
-        date = df.index
+                up_rate = 100 * (max_value - pre) / pre
+                duration = idxmax - date[0]
+                up_rates.append(up_rate)
+                durations.append(duration)
+                cross.append(date[0])
+                peak.append(idxmax)
+                    
+        else:
+            for i in range(len(up)):
+                date = df.index[up[i]:down[i]]
+                ac = df[key][up[i]:down[i]]
+                pre = ac[0]
+                max_value = ac.max()
+                idxmax = ac.idxmax()
+
+                up_rate = 100 * (max_value - pre) / pre
+                duration = idxmax - date[0]
+                up_rates.append(up_rate)
+                durations.append(duration)
+                cross.append(date[0])
+                peak.append(idxmax)
+                
+        df2 = pd.DataFrame({
+            "up_rate": up_rates,
+            "cross_date": cross,
+            "peak_date": peak,
+            "duration": durations})
+        
+        return (df2, md)
+   
+    def peak_rate_down(self, md=10, key="Adj Close"):
+            up, down, md = self.cross_point(md)
+            df = self.df
+            down_rates = []
+            durations = []
+            cross = []
+            peak = []
+            if up[0] > down[0]:   
+                for i in range(len(down)):
+
+                    date = df.index[down[i]:up[i+1]]
+                    ac = df[key][down[i]:up[i+1]]
+                    pre = ac[0]
+                    min_value = ac.min()
+                    idxmax = ac.idxmin()
+
+                    down_rate = 100 * (pre - min_value) / pre
+                    duration = idxmin - date[0]
+                    down_rates.append(down_rate)
+                    durations.append(duration)
+                    cross.append(date[0])
+                    peak.append(idxmax)
+
+            else:
+                for i in range(len(up)):
+                    date = df.index[down[i]:up[i]]
+                    ac = df[key][down[i]:up[i]]
+                    pre = ac[0]
+                    min_value = ac.min()
+                    idxmin = ac.idxmin()
+
+                    down_rate = 100 * (min_value - pre) / pre
+                    duration = idxmin - date[0]
+                    down_rates.append(down_rate)
+                    durations.append(duration)
+                    cross.append(date[0])
+                    peak.append(idxmin)
+
+            df2 = pd.DataFrame({
+                "down_rate": down_rates,
+                "cross_date": cross,
+                "peak_date": peak,
+                "duration": durations})
+
+            return (df2, md)
     
-        for i in range(len(df["Adj Close"])-1):
-            x = df["Adj Close"][i+1] - df["Adj Close"][i]
-            rate = 100 * x / df["Adj Close"][i+1]
-            tren.append(x)
-            raise_rate.append(rate)
-        
-        for i in range(len(df["High"])):
-            y = df["High"][i] - df["Low"][i]
-            widt.append(y)
-                                            
-        df["trend"] = tren
-        df["width"] = widt
-        df["raise rate"] = raise_rate
-        
-        width_mean = df["width"].mean() 
-        width_max = df["width"].max()
-        width_min = df["width"].min()
-        
-        trend_mean = df["trend"].mean()
-        trend_max = df["trend"].max()
-        trend_min = df["trend"].min()
-        
-        raise_rate_mean = df["raise rate"].mean()
-        raise_rate_max = df["raise rate"].max()
-        raise_rate_min = df["raise rate"].min()
-            
-        plt.figure(figsize=(30,15))
-        plt.subplot(2,1,1)
-        plt.bar(date, df["raise rate"], label='raise rate comparing with each previous day [%]', color='b')
-        plt.title("raise rate comparing with each previous day [%]", fontsize=18)
-        plt.xlabel("date", fontsize=18)
-        plt.ylabel("[%]", fontsize=18)
-        plt.legend(fontsize=18)
-                   
-        plt.subplot(2,1,2)
-        plt.bar(date, df["width"], 
-                label='Daily difference between the highest price and the lowest one [yen]', color='b')
-        plt.title("Daily difference between the highest price and the lowest one [yen]")
-        plt.xlabel("date", fontsize=18)
-        plt.ylabel("[yen]", fontsize=18)
-        plt.legend(fontsize=18)
-        
-        if self.showfig == True:
-            plt.show()
-        if self.savefig == True:
-            plt.savefig(self.path + '/' + self.name + '/' + '-raise_rate' + '.png')
+    def floor_dif(self, df=None, key=None):
+        if df is None:
+            df = self.df
+        if key is None:
+            key = "Adj Close"
+        x = df[key]
+        array = []
+        for i in range(1, len(x)):
+            array.append(x[i]-x[i-1])
+        df2 = pd.DataFrame({"floor_dif": array}, index=df.index[1:])
+        return df2
     
-def main():
-    x = Stock()
-    x.graph()
-    x.diff()
+    def ndim_floor_dif(self, n, df=None, key=None):
+        if df is None:
+            df = self.df
+        if key is None:
+            key = "Adj Close"
+        x = df[key]
         
-if __name__ == '__main__':
-    main()
+        if type(n) is not int:
+            print('The type of "n" must be int.')
+            return
+        elif len(df) < n + 1:
+            print('The length of array is smaller than or equal to n')
+            return
+        else:
+            df2 = self.floor_dif(df=df, key=key)
+            for i in range(n-1):
+                df2 = self.floor_dif(df=df2, key=df2.columns[0])
+            return df2
